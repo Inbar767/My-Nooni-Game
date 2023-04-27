2beb2a9cc5cfb9b2da79f5325297aa3597a9854f
Inbar767 <118209251+Inbar767@users.noreply.github.com>
2023-04-27 22:30:12 +0300

    Update EffectsControl.cpp

--- EffectsControl.cpp
+++ EffectsControl.cpp
@@ -4,7 +4,7 @@
 
 //Constructor gets a data
 EffectsControl::EffectsControl(gameDataRef data) : 
-	data(data), currentEffect(EffectType::NO_EFFECT), countAnimation(0)
+	data(data), currentEffect(EffectsControl::EffectType::NO_EFFECT), countAnimation(0)
 {
 	addEffects();
 }
@@ -33,22 +33,22 @@ void EffectsControl::addEffects()
 	effects.push_back(std::make_unique <Animation>(data, BABY_GROWING_ANIMATION, 10, sf::Vector2f(data->window.getSize().x / 2 - 96, 370)));
 
 	//Addings actions
-	effects.at(static_cast<int>(EffectType::SLEEP_EFFECT))->addAction("effect", 0, 22, 0.04, false);
-	effects.at(static_cast<int>(EffectType::BATH_EFFECT))->addAction("effect", 0, 25, 0.09, false);
+	effects.at(static_cast<int>(EffectsControl::EffectType::SLEEP_EFFECT))->addAction("effect", 0, 22, 0.04, false);
+	effects.at(static_cast<int>(EffectsControl::EffectType::BATH_EFFECT))->addAction("effect", 0, 25, 0.09, false);
 
-	for (int foodIndex = static_cast<int>(EffectType::MILK_EFFECT); foodIndex <= static_cast<int>(EffectType::CHOCOLATE_EFFECT); foodIndex++)
+	for (int foodIndex = static_cast<int>(EffectsControl::EffectType::MILK_EFFECT); foodIndex <= static_cast<int>(EffectsControl::EffectType::CHOCOLATE_EFFECT); foodIndex++)
 	{
 		effects.at(foodIndex)->addAction("effect", 0, 3, 2.0f, false);
 	}
 
-	for (int toyIndex = static_cast<int>(EffectType::BALL_EFFECT); toyIndex <= static_cast<int>(EffectType::CAR_EFFECT); toyIndex++)
+	for (int toyIndex = static_cast<int>(EffectsControl::EffectType::BALL_EFFECT); toyIndex <= static_cast<int>(EffectsControl::EffectType::CAR_EFFECT); toyIndex++)
 	{
 		effects.at(toyIndex)->addAction("effect", 0, 9, 0.05f, true);
 	}
 
-	effects.at(static_cast<int>(EffectType::PET_EFFECT))->addAction("effect", 0, 7, 0.09f, false);
+	effects.at(static_cast<int>(EffectsControl::EffectType::PET_EFFECT))->addAction("effect", 0, 7, 0.09f, false);
 
-	for (int growIndex = static_cast<int>(EffectType::GROW_EGG_EFFECT); growIndex <= static_cast<int>(EffectType::GROW_BABY_EFFECT); growIndex++)
+	for (int growIndex = static_cast<int>(EffectsControl::EffectType::GROW_EGG_EFFECT); growIndex <= static_cast<int>(EffectsControl::EffectType::GROW_BABY_EFFECT); growIndex++)
 	{
 		effects.at(growIndex)->addAction("effect", 0, 9, 0.2f, false);
 	}
@@ -57,7 +57,7 @@ void EffectsControl::addEffects()
 //Draws current effect
 void EffectsControl::draw()
 {
-	if (currentEffect != EffectType::NO_EFFECT)
+	if (currentEffect != EffectsControl::EffectType::NO_EFFECT)
 	{
 		effects.at(currentEffect)->draw();
 	}
@@ -66,14 +66,14 @@ void EffectsControl::draw()
 //Updates current effect frames
 void EffectsControl::update()
 {
-	if (currentEffect != EffectType::NO_EFFECT)
+	if (currentEffect != EffectsControl::EffectType::NO_EFFECT)
 	{
-		if(effects.at(currentEffect)->animation() && currentEffect != EffectType::PET_EFFECT)
+		if(effects.at(currentEffect)->animation() && currentEffect != EffectsControl::EffectType::PET_EFFECT)
 		{
 			countAnimation++;
 			if (countAnimation == maxAnimation) //If current effect is animated maxAnimation times - stop effect
 			{
-				currentEffect = EffectType::NO_EFFECT;
+				currentEffect = EffectsControl::EffectType::NO_EFFECT;
 				countAnimation = 0;
 			}
 		}
@@ -81,21 +81,21 @@ void EffectsControl::update()
 }
 
 //Playes effect
-void EffectsControl::startEffect(EffectType effectNumber)
+void EffectsControl::startEffect(EffectsControl::EffectType effectNumber)
 {
 	int effectIndex = static_cast<int>(effectNumber);
-	if (effectNumber >= EffectType::SLEEP_EFFECT && effectIndex < effects.size())
+	if (effectNumber >= EffectsControl::EffectType::SLEEP_EFFECT && effectIndex < effects.size())
 	{
 		currentEffect = effectNumber;
 		countAnimation = 0;
 		//Changes maxAnimation accordding to type of the effect
-		if (effectNumber == EffectType::SLEEP_EFFECT)
+		if (effectNumber == EffectsControl::EffectType::SLEEP_EFFECT)
 		{
 			maxAnimation = 5;
 		}
 		else
 		{
-			if (effectNumber == EffectType::BATH_EFFECT)
+			if (effectNumber == EffectsControl::EffectType::BATH_EFFECT)
 			{
 				maxAnimation = 2;
 			}
@@ -111,22 +111,22 @@ void EffectsControl::startEffect(EffectType effectNumber)
 //Sends if effects is being played
 bool EffectsControl::isEffect()
 {
-	return (currentEffect != EffectType::NO_EFFECT);
+	return (currentEffect != EffectsControl::EffectType::NO_EFFECT);
 }
 
 //Stops current effect
 void EffectsControl::stopEffect()
 {
-	if (currentEffect != EffectType::NO_EFFECT)
+	if (currentEffect != EffectsControl::EffectType::NO_EFFECT)
 	{
-		currentEffect = EffectType::NO_EFFECT;
+		currentEffect = EffectsControl::EffectType::NO_EFFECT;
 	}
 }
 
 //Restarts clock of current effect
 void EffectsControl::restartClock()
 {
-	if (currentEffect != EffectType::NO_EFFECT)
+	if (currentEffect != EffectsControl::EffectType::NO_EFFECT)
 	{
 		int effectIndex = static_cast<int>(currentEffect);
 		effects.at(effectIndex)->restartClock();
