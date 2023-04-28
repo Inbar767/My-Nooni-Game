4d56dbfcd593a32e08d63681ec41a2ab7001af77
Inbar767 <118209251+Inbar767@users.noreply.github.com>
2023-04-28 12:40:54 +0300

    Update SelectState.cpp

--- SelectState.cpp
+++ SelectState.cpp
@@ -1,6 +1,5 @@
 #include "SelectState.h"
 #include "Level1.h"
-#include <sstream>;
 #include <iostream>
 
 //Constructor gets a data and sounds
@@ -19,22 +18,21 @@ void SelectState::init()
 	windowSprite.setTexture(data->assets.getTexture("Window Background"));
 	windowSprite.setPosition(sf::Vector2f(data->window.getSize().x / 2 - windowSprite.getGlobalBounds().width / 2,
 		data->window.getSize().y / 2 - windowSprite.getGlobalBounds().height / 2));
-
-	//Init eggs buttons
-	data->assets.loadTexture("Select Menu Bunny Button", ANGEL_FOLDER + std::to_string(0) + ".png");
-	animalButtons[0].setTexture(data->assets.getTexture("Select Menu Bunny Button"));
-	data->assets.loadTexture("Select Menu Fox Button", COCO_FOLDER + std::to_string(0) + ".png");
-	animalButtons[1].setTexture(data->assets.getTexture("Select Menu Fox Button"));
-	data->assets.loadTexture("Select Menu Penguin Button", FIFI_FOLDER + std::to_string(0) + ".png");
-	animalButtons[2].setTexture(data->assets.getTexture("Select Menu Penguin Button"));
-	data->assets.loadTexture("Select Menu Pug Button", BOO_FOLDER + std::to_string(0) + ".png");
-	animalButtons[3].setTexture(data->assets.getTexture("Select Menu Pug Button"));
-
 	data->assets.loadTexture("Select Menu Back Button", BACK_BUTTON);
 	backButton.setTexture(data->assets.getTexture("Select Menu Back Button"));
 	backButton.setPosition(sf::Vector2f(150,500));
 	data->assets.loadTexture("Menu Effect", SELECT_EFFECT);
 	effect.setTexture(data->assets.getTexture("Menu Effect"));
+
+	//Init eggs buttons
+	data->assets.loadTexture("Select Menu Bunny Button", ANGEL_FOLDER + std::to_string(0) + ".png");
+	eggButtons[0].setTexture(data->assets.getTexture("Select Menu Bunny Button"));
+	data->assets.loadTexture("Select Menu Fox Button", COCO_FOLDER + std::to_string(0) + ".png");
+	eggButtons[1].setTexture(data->assets.getTexture("Select Menu Fox Button"));
+	data->assets.loadTexture("Select Menu Penguin Button", FIFI_FOLDER + std::to_string(0) + ".png");
+	eggButtons[2].setTexture(data->assets.getTexture("Select Menu Penguin Button"));
+	data->assets.loadTexture("Select Menu Pug Button", BOO_FOLDER + std::to_string(0) + ".png");
+	eggButtons[3].setTexture(data->assets.getTexture("Select Menu Pug Button"));
 
 	//Position eggs
 	int  y = Y_FIRST_POSITION, x = X_FIRST_POSITION;
@@ -52,7 +50,7 @@ void SelectState::init()
 		{
 			y += DIST_EGG_HEIGHT;
 		}
-		animalButtons[eggIndex].setPosition(sf::Vector2f(x, y));
+		eggButtons[eggIndex].setPosition(sf::Vector2f(x, y));
 	}
 	//Loads font for text
 	if (!font.loadFromFile("fonts/retganon.ttf"))
@@ -63,12 +61,12 @@ void SelectState::init()
 	{
 		for (int i = 0; i < NUMBER_EGG; i++)
 		{
-			text[i].setString(static_cast<std::string>(EGGS_NAMES[i]));
-			text[i].setFont(font);
-			text[i].setCharacterSize(25);
-			text[i].setFillColor(sf::Color{ 249, 85, 162, 255 });
-			text[i].setPosition(sf::Vector2f(animalButtons[i].getGlobalBounds().getPosition().x + 20,
-				animalButtons[i].getGlobalBounds().getPosition().y + animalButtons[i].getGlobalBounds().height + 2));
+			eggName[i].setString(static_cast<std::string>(EGGS_NAMES[i]));
+			eggName[i].setFont(font);
+			eggName[i].setCharacterSize(25);
+			eggName[i].setFillColor(sf::Color{ 249, 85, 162, 255 });
+			eggName[i].setPosition(sf::Vector2f(eggButtons[i].getGlobalBounds().getPosition().x + 20,
+				eggButtons[i].getGlobalBounds().getPosition().y + eggButtons[i].getGlobalBounds().height + 2));
 		}
 	}
 }
@@ -82,42 +80,34 @@ bool SelectState::handleInput(sf::Event event)
 	}
 	else
 	{
-		if (!isEffect)
+		if (data->input.isSpriteClicked(backButton, sf::Mouse::Left, data->window))
 		{
-			//Checks if the mouse touches the egg in order to add effect
-			for (int i = 0; i < NUMBER_EGG && !isEffect; i++)
+			sounds->playGameSound(SoundManage::SoundGameType::MOUSE_CLICKED_SOUND);
+			data->machine.removeState();
+		}
+		else if (isEffect && !eggButtons[effectTouch].getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(data->window).x, sf::Mouse::getPosition(data->window).y)))
+		{
+			isEffect = false;
+		}
+		else
+		{	
+			for (int eggIndex = 0; eggIndex < NUMBER_EGG; eggIndex++)
 			{
-				if (animalButtons[i].getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(data->window).x, sf::Mouse::getPosition(data->window).y)))
+				if (data->input.isSpriteClicked(eggButtons[eggIndex], sf::Mouse::Left, data->window))
+				{
+					Game::startNewGame();
+					sounds->playGameSound(SoundManage::SoundGameType::HAPPY_PET_SOUND);
+					data->machine.addState(StateRef(new Level1(data, sounds, static_cast<Pet::NooniName>(eggIndex))), true);
+				}
+				if (eggButtons[eggIndex].getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(data->window).x, sf::Mouse::getPosition(data->window).y)))
 				{
 					isEffect = true;
-					effect.setPosition(sf::Vector2f(animalButtons[i].getPosition().x - 14, animalButtons[i].getPosition().y - 17));
-					effectTouch = i;
+					effect.setPosition(sf::Vector2f(eggButtons[eggIndex].getPosition().x - 14, eggButtons[eggIndex].getPosition().y - 17));
+					effectTouch = eggIndex;
+					break;
 				}
 			}
 		}
-		else
-		{
-			if (!animalButtons[effectTouch].getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(data->window).x, sf::Mouse::getPosition(data->window).y)))
-			{
-				isEffect = false;
-			}
-		}
-		//Checks if one egg is being selected and starts new game
-		for (int eggIndex = 0; eggIndex < NUMBER_EGG; eggIndex++)
-		{
-			if (data->input.isSpriteClicked(animalButtons[eggIndex], sf::Mouse::Left, data->window))
-			{
-				Game::startNewGame();
-				sounds->playGameSound(SoundManage::SoundGameType::HAPPY_PET_SOUND);
-				data->machine.addState(StateRef(new Level1(data, sounds, static_cast<Pet::NooniName>(eggIndex))), true);
-				return true;
-			}
-		}
-	}
-	if (data->input.isSpriteClicked(backButton, sf::Mouse::Left, data->window))
-	{
-		sounds->playGameSound(SoundManage::SoundGameType::MOUSE_CLICKED_SOUND);
-		data->machine.removeState();
 	}
 	return true;
 }
@@ -125,7 +115,7 @@ bool SelectState::handleInput(sf::Event event)
 //Updates pam's speech
 void SelectState::update(float dt)
 {
-	if (pam->getIsDone())
+	if (isInstruction && pam->getIsDone())
 	{
 		isInstruction = false;
 	}
@@ -136,18 +126,21 @@ void SelectState::draw(float dt)
 {
 	data->window.draw(background);
 	data->window.draw(windowSprite);
-	if (isEffect)
-	{
-		data->window.draw(effect);
-	}
-	for (int i = 0; i < NUMBER_EGG; i++)
-	{
-		data->window.draw(animalButtons[i]);
-		data->window.draw(text[i]);
-	}
 	if (isInstruction)
 	{
 		pam->draw();
+	}
+	else
+	{
+		if (isEffect)
+		{
+			data->window.draw(effect);
+		}
+		for (int eggIndex = 0; eggIndex < eggButtons.size(); eggIndex++)
+		{
+			data->window.draw(eggButtons[eggIndex]);
+			data->window.draw(eggName[eggIndex]);
+		}
 	}
 	data->window.draw(backButton);
 }
